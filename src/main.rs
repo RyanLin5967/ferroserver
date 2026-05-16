3e93c8c984100ce0374ae1a71a09cd10ba2f59c6
RyanLin5967 <idident5967@gmail.com>
2026-05-15 23:27:17 -0400

    added reverse proxy testing code

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,19 @@ mod router;
 mod handler;
 mod connection;
 mod compression;
+mod proxy;
 use crate::handler::{echo, health};
 use crate::request::Method;
 use crate::router::Router;
 use std::path::PathBuf;
 use std::sync::Arc;
 
-// ***NEED TO REFACTOR TO ASYNC FROM SYNC ***
 #[tokio::main]
 async fn main(){
     let path = PathBuf::from("./public");
     let mut router = Router::new(path);
     router.add(Method::GET, String::from("/api/health"), health);
     router.add(Method::POST, String::from("/api/echo"), echo);
+    router.add_proxy(String::from("/proxy"), String::from("127.0.0.1:3001"));
     server::run("127.0.0.1:8000", Arc::new(router)).await;
 }
--- src/router.rs
+++ src/router.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use crate::{error::ServerError, request::{HttpRequest, Method}, response::HttpResponse, static_files::serve_file};
+use crate::{error::ServerError, proxy::forward, request::{HttpRequest, Method}, response::HttpResponse, static_files::serve_file};
 
 pub struct Route {
     method: Method,
@@ -10,6 +10,7 @@ pub struct Route {
 
 pub struct Router {
     routes: Vec<Route>,
+    proxy_routes: Vec<(String, String)>,
     root_path: PathBuf
 }
 
@@ -18,11 +19,23 @@ impl Router {
         self.routes.push(Route{method, pattern, handler})
     }
 
+    pub fn add_proxy(&mut self, pattern: String, backend: String){
+        self.proxy_routes.push((pattern, backend));
+    }
+
     pub fn new(static_root: PathBuf) -> Router {
-        return Router { routes: Vec::new(), root_path: static_root }
+        return Router { routes: Vec::new(), root_path: static_root, proxy_routes: Vec::new() }
     }
 
     pub async fn dispatch(&self, request: HttpRequest) -> HttpResponse{
+        for (pattern, backend) in &self.proxy_routes {
+            if request.path.starts_with(pattern) {
+                let res = match forward(request, backend).await {
+                    Ok(resp) => return resp,
+                    Err(e) => return HttpResponse::from_error(&e)
+                };
+            }
+        }
         for route in &self.routes {
             if route.method == request.method && route.pattern == request.path {
                 return match (route.handler)(request) {
